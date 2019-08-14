f879a8903b322c823cb2cc23d0884e32511c06b2
Alejandro Mesias <amesias@l:wqocalhost.localdomain>
2019-08-13 23:19:53 -0300

    WIP Recicler

--- app/src/main/java/com/desafioconcret/MainActivity.java
+++ app/src/main/java/com/desafioconcret/MainActivity.java
@@ -41,11 +41,11 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
     private Integer page = 1;
     private EndlessScroll endlessScroll;
-    private RecyclerView.Adapter adapter;
+    private GitAdapter adapter;
     GitHubApiService gitHubApiService = new GitHubApiService();
 
 
-    private List<Repositories> repositorios = new ArrayList<>();
+    // private List<Repositories> repositorios = new ArrayList<>();
     private Toolbar toolbar;
 
     private EndlessScroll scrollListener;
@@ -56,15 +56,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.adapter = new GitAdapter(this.repositorios);
         this.layoutManager = new LinearLayoutManager(this);
-
 
         this.recyclerView = findViewById(R.id.repos_recicle_view);
         this.recyclerView.setHasFixedSize(true);
         this.recyclerView.setLayoutManager(layoutManager);
         this.recyclerView.setAdapter(adapter);
         this.recyclerView.addItemDecoration(new SimpleDivider(this));
+
+        adapter = new GitAdapter();
+        recyclerView.setAdapter(adapter);
 
         toolbar = findViewById(R.id.toolbar_pullrequest);
         setSupportActionBar(toolbar);
@@ -102,8 +103,9 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void run() {
 
-                        repositorios.addAll(response.body().getRepositories());
-                        recyclerView.setAdapter(adapter);
+                        //repositorios.addAll(response.body().getRepositories());
+                        List<Repositories> repos = response.body().getRepositories();
+                        adapter.addRepositories(repos);
                         adapter.notifyDataSetChanged();
 
                     }
